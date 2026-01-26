14e5a51f7acaf3b94eaa2bd3dc5dbfc0c6bccbc6
Michael Berezovsky <mishka@Michaels-Laptop.lan>
2026-01-27 01:21:47 +0200

    Add logger configuration using pino, update config

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,9 @@
 import type { Config } from "./types/config";
 
-export const config:Config = {
-    redisHost: process.env.REDIS_HOST || "localhost",
-    redisPort: Number(process.env.REDIS_PORT) || 6379
-}
+export const config: Config = {
+	port: Number(process.env.PORT) || 3000,
+    logLevel: process.env.LOG_LEVEL || "info",
+	redisHost: process.env.REDIS_HOST || "localhost",
+	redisPort: Number(process.env.REDIS_PORT) || 6379,
+	queueName: process.env.QUEUE_NAME || "job-queue",
+};
